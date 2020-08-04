85cc028102d2eadea9adb6e56c7b1326ad2d97cf
kyle-cloud <1139467635@qq.com>
2020-08-04 20:16:40 +0800

    调试了一下

--- leetcode/practice.py
+++ leetcode/practice.py
@@ -1,33 +1,26 @@
 # -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 class Solution:
-    # s字符串
-    def isNumeric(self, s):
+    # 返回二维列表，内部每个列表表示找到的路径
+    def __init__(self):
+        self.ans = []
+        
+    def FindPath(self, root, expectNumber):
         # write code here
-        # +/-A[.B][e|E C]
-        # +/-.B[w|E C]
-        if s == None or len(s) == 0: return False
+        if root == None: return False
         
-        index = 0
-        if s[0] == '+' or s[0] == '-': 
-            index += 1
-        #A
-        index = self.scan(s, index)
-        #.B
-        if index < len(s) and s[index] == '.':
-            index += 1
-            index = self.scan(s, index)
-        #e|E C
-        if index < len(s) and (s[index] == 'E' or s[index] == 'e'):
-            index += 1
-            if index < len(s) and s[index] == '+' or s[index] == '-': 
-                index += 1
-            index = self.scan(s, index)
+        self.DFS(root, expectNumber, [root], root.val)
+        return self.ans
+    
+    def DFS(self, root, expectNumber, path, curNumber):
+        if root.left == None and root.right == None:
+            if curNumber == expectNumber:
+                self.ans.append(path)
+            return
         
-        return True if index == len(s) else False
-     
-    def scan(self, s, index):
-        while index < len(s):
-            if s[index] < '0' or s[index] > '9':
-                break
-            index += 1
-        return index
+        if root.left: self.DFS(root.left, expectNumber, path + [root.left], curNumber + root.left.val)
+        if root.right: self.DFS(root.right, expectNumber, path + [root.right], curNumber + root.right.val)

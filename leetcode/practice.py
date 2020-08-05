c8c19c2713b85ddd3134128759cb0da381846ac1
kyle-cloud <1139467635@qq.com>
2020-08-05 20:30:26 +0800

    联系

--- leetcode/practice.py
+++ leetcode/practice.py
@@ -1,26 +1,22 @@
 # -*- coding:utf-8 -*-
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
 class Solution:
-    # 返回二维列表，内部每个列表表示找到的路径
     def __init__(self):
         self.ans = []
-        
-    def FindPath(self, root, expectNumber):
+    
+    def Permutation(self, ss):
         # write code here
-        if root == None: return False
+        if not ss: return []
         
-        self.DFS(root, expectNumber, [root], root.val)
+        self.DFS(ss, '')
         return self.ans
     
-    def DFS(self, root, expectNumber, path, curNumber):
-        if root.left == None and root.right == None:
-            if curNumber == expectNumber:
-                self.ans.append(path)
+    def DFS(self, s, res):
+        
+        if s == "":
+            print(s + '1' + res)
+            self.ans.append(res)
             return
         
-        if root.left: self.DFS(root.left, expectNumber, path + [root.left], curNumber + root.left.val)
-        if root.right: self.DFS(root.right, expectNumber, path + [root.right], curNumber + root.right.val)
+        for i in range(len(s)):
+            self.DFS(s[:i] + s[i+1:], res + s[i])
+        

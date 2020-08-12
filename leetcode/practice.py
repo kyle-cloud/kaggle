161139404f0d1d798129f93c80ba25d349c63a62
kyle-cloud <1139467635@qq.com>
2020-08-12 19:46:57 +0800

    做完了今天的5道

--- leetcode/practice.py
+++ leetcode/practice.py
@@ -1,15 +1,29 @@
 # -*- coding:utf-8 -*-
-import heapq
-
 class Solution:
-    def GetLeastNumbers_Solution(self, tinput, k):
+    def NumberOf1Between1AndN_Solution(self, n):
         # write code here
-        heap = []
-        for num in tinput:
-            if len(heap) < k:
-                heapq.heappush(heap, num)
+        if n < 0: return False
+        if n == 0: return 0
+        
+        flag = 1
+        ans = 0
+        s = str(n)
+        length = len(s)
+        for i in range(length):
+            if i == length - 1:
+                number = int(s[:length-1])
+                if int(s[i]) >= flag: number += 1
+                ans += number
+            elif i == 0:
+                number = int(s[1:])
+                if int(s[i]) >= flag: number += 1
+                ans += number
             else:
-                item_min = heap[0]
-                if item_min < num:
-                    heapq.heapreplace(heap, num)
-        return heap
+                number_left = int(s[:i])
+                number_right = int(s[i+1:])
+                if int(s[i]) < flag:
+                    ans += number_left * (pow(10, length - i - 1))
+                elif int(s[i]) == flag:
+                    ans += number_left * (pow(10, length - i - 1))
+                    ans += numebr_right + 1
+        return ans

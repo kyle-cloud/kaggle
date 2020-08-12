d29368d64d755f53e7bb0df47cd34f1ca1833591
kyle-cloud <1139467635@qq.com>
2020-08-12 09:46:01 +0800

    最小 最大堆

--- leetcode/practice.py
+++ leetcode/practice.py
@@ -1,34 +1,15 @@
 # -*- coding:utf-8 -*-
+import heapq
+
 class Solution:
-    def MoreThanHalfNum_Solution(self, numbers):
+    def GetLeastNumbers_Solution(self, tinput, k):
         # write code here
-        if numbers == None or numbers == []: return None
-        if len(numbers) == 1: return numbers[0]
-        # find the medium
-        res = self.findTheMedium(numbers, 0, len(numbers) - 1)
-        
-        cnt = 0
-        for num in numbers:
-            if num == res:
-                cnt += 1
-        return res if cnt > len(numbers) >> 1 else 0
-    
-    def findTheMedium(self, numbers, start, end):
-        medium = len(numbers) >> 1
-        index = self.partion(numbers, start, end)
-        while index != medium:
-            if index > medium and start < index - 1:
-                index = self.partion(numbers, start, index - 1)
-            elif index < medium and index + 1 < end:
-                index = self.partion(numbers, index + 1, end)
-        return numbers[index]
-    
-    def partion(self, numbers, start, end):
-        pivot = numbers[start]
-        while start < end:
-            while start < end and numbers[end] >= pivot: end -= 1
-            numbers[start] = numbers[end]
-            while start < end and numbers[start] <= pivot: start += 1
-            numbers[end] = numbers[start]
-        numbers[start] = pivot
-        return start
+        heap = []
+        for num in tinput:
+            if len(heap) < k:
+                heapq.heappush(heap, num)
+            else:
+                item_min = heap[0]
+                if item_min < num:
+                    heapq.heapreplace(heap, num)
+        return heap

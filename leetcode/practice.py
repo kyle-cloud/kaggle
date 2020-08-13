f24110c837e3459972bb18fc59b7c2c0b2d46fa3
kyle-cloud <1139467635@qq.com>
2020-08-13 16:45:44 +0800

    完成了今日的练习

--- leetcode/practice.py
+++ leetcode/practice.py
@@ -1,29 +1,18 @@
 # -*- coding:utf-8 -*-
 class Solution:
-    def NumberOf1Between1AndN_Solution(self, n):
+    def FirstNotRepeatingChar(self, s):
         # write code here
-        if n < 0: return False
-        if n == 0: return 0
+        if not s: return -1
         
-        flag = 1
-        ans = 0
-        s = str(n)
-        length = len(s)
-        for i in range(length):
-            if i == length - 1:
-                number = int(s[:length-1])
-                if int(s[i]) >= flag: number += 1
-                ans += number
-            elif i == 0:
-                number = int(s[1:])
-                if int(s[i]) >= flag: number += 1
-                ans += number
+        dic = {}
+        for ch in s:
+            if ch not in dic:
+                dic[ch] = 1
             else:
-                number_left = int(s[:i])
-                number_right = int(s[i+1:])
-                if int(s[i]) < flag:
-                    ans += number_left * (pow(10, length - i - 1))
-                elif int(s[i]) == flag:
-                    ans += number_left * (pow(10, length - i - 1))
-                    ans += numebr_right + 1
-        return ans
+                dic[ch] = dic[ch] + 1
+        
+        for key, value in dic.items():
+            if value == 1:
+                return s.index(key)
+        
+        return -1

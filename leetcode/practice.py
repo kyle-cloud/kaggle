55f2ac978f977cf9aac24ad4229e2e96b47661ae
kyle-cloud <1139467635@qq.com>
2020-08-16 20:51:52 +0800

    刷完了剑指offer

--- leetcode/practice.py
+++ leetcode/practice.py
@@ -1,21 +1,33 @@
 # -*- coding:utf-8 -*-
 class Solution:
-    def FindContinuousSequence(self, tsum):
+    def __init__(self):
+        self.minus = 1 #-1
+        self.g_valid = False
+        
+    def StrToInt(self, s):
         # write code here
-        ans = []
-        small, big = 1, 2
-        mid = tsum >> 1
-        curSum = 3
-        res = [small, big]
-        while small <= mid:
-            print(curSum, res, ans)
-            if curSum >= tsum:
-                if curSum == tsum: ans.append(res.copy())
-                curSum -= small
-                small += 1
-                res.pop(0)
-            elif curSum < tsum:
-                big += 1
-                curSum += big
-                res.append(big)
-        return ans
+        # None "" + a overflow
+        if not s: self.g_valid = False; return 0
+        if len(s) == 1 and (s[0] == '+' or s[0] == '-'):
+            self.g_valid = False; return
+        
+        num = 0
+        index = 0
+        if s[index] == '+':
+            index += 1
+            self.minus = 1
+        elif s[index] == '-':
+            index += 1
+            self.minus = -1
+        
+        while index < len(s):
+            if s[index] < '0' or s[index] > '9':
+                self.g_valid = False
+                return
+            num = num * 10 + self.minus * int(s[index])
+            if num > 0x7FFFFFFF or num < 0x80000000:
+                index += 1
+            else:
+                self.g_valid = False
+                return
+        return num

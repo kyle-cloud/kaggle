1405849808f343c0a8b74dd39788564937a9f82d
kyle-cloud <1139467635@qq.com>
2020-08-18 11:45:12 +0800

    练习了quickSort

--- leetcode/practice.py
+++ leetcode/practice.py
@@ -1,33 +1,17 @@
 # -*- coding:utf-8 -*-
 class Solution:
-    def __init__(self):
-        self.minus = 1 #-1
-        self.g_valid = False
-        
-    def StrToInt(self, s):
-        # write code here
-        # None "" + a overflow
-        if not s: self.g_valid = False; return 0
-        if len(s) == 1 and (s[0] == '+' or s[0] == '-'):
-            self.g_valid = False; return
-        
-        num = 0
-        index = 0
-        if s[index] == '+':
-            index += 1
-            self.minus = 1
-        elif s[index] == '-':
-            index += 1
-            self.minus = -1
-        
-        while index < len(s):
-            if s[index] < '0' or s[index] > '9':
-                self.g_valid = False
-                return
-            num = num * 10 + self.minus * int(s[index])
-            if num > 0x7FFFFFFF or num < 0x80000000:
-                index += 1
-            else:
-                self.g_valid = False
-                return
-        return num
+    def quickSort(self, arr, left, right):
+        if left < right:
+            index = self.partion(arr, left, right)
+            self.quickSort(arr, left, index)
+            self.quickSort(arr, index + 1, right)
+    
+    def partion(self, arr, left, right):
+        pivot = arr[left]
+        while left < right:
+            while left < right and arr[right] <= pivot: right -= 1
+            arr[left] = arr[right]
+            while left < right and arr[left] >= pivot: left += 1
+            arr[right] = arr[left]
+        arr[left] = pivot
+        return left

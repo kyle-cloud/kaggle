893c7da89093e809fc42010c83fffa8c877948c9
kyle-cloud <1139467635@qq.com>
2020-08-24 22:00:55 +0800

    练习

--- leetcode/practice.py
+++ leetcode/practice.py
@@ -1,17 +1,29 @@
-# -*- coding:utf-8 -*-
+from typing import List
 class Solution:
-    def quickSort(self, arr, left, right):
-        if left < right:
-            index = self.partion(arr, left, right)
-            self.quickSort(arr, left, index)
-            self.quickSort(arr, index + 1, right)
+    import sys
+    sys.setrecursionlimit(1000000)
     
-    def partion(self, arr, left, right):
-        pivot = arr[left]
-        while left < right:
-            while left < right and arr[right] <= pivot: right -= 1
-            arr[left] = arr[right]
-            while left < right and arr[left] >= pivot: left += 1
-            arr[right] = arr[left]
-        arr[left] = pivot
-        return left
+    def __init__(self):
+        self.directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        self.ans = False
+    
+    def containsCycle(self, grid: List[List[str]]) -> bool:
+        if not grid: return False
+        if len(grid) == 1: return False
+
+        visited = [[False] * len(grid[0]) for _ in range(len(grid))]
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if not visited[i][j]:
+                    self.dfs(grid, visited, i, j, [-1, -1])
+                    if self.ans: return True
+        return False
+    
+    def dfs(self, grid, visited, row, col, father):
+        visited[row][col] = True
+        for i, j in self.directions:
+            if 0 <= row + i < len(grid) and 0 <= col + j < len(grid[0]) and (row + i != father[0] or col + j != father[1]) and grid[row + i][col + j] == grid[row][col]:
+                if visited[row+i][col+j]:
+                    self.ans = True
+                    return
+                self.dfs(grid, visited, row+i, col+j, [row, col])

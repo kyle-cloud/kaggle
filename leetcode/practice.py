3056cc9da42fc34e98d2a83065fc16eb7baffaf1
kyle-cloud <1139467635@qq.com>
2020-09-25 22:10:39 +0800

    做了个学校oj题，时隔两年再用坑太多了

--- leetcode/practice.py
+++ leetcode/practice.py
@@ -1,29 +1,14 @@
-from typing import List
-class Solution:
-    import sys
-    sys.setrecursionlimit(1000000)
-    
-    def __init__(self):
-        self.directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-        self.ans = False
-    
-    def containsCycle(self, grid: List[List[str]]) -> bool:
-        if not grid: return False
-        if len(grid) == 1: return False
+import math
 
-        visited = [[False] * len(grid[0]) for _ in range(len(grid))]
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if not visited[i][j]:
-                    self.dfs(grid, visited, i, j, [-1, -1])
-                    if self.ans: return True
-        return False
-    
-    def dfs(self, grid, visited, row, col, father):
-        visited[row][col] = True
-        for i, j in self.directions:
-            if 0 <= row + i < len(grid) and 0 <= col + j < len(grid[0]) and (row + i != father[0] or col + j != father[1]) and grid[row + i][col + j] == grid[row][col]:
-                if visited[row+i][col+j]:
-                    self.ans = True
-                    return
-                self.dfs(grid, visited, row+i, col+j, [row, col])
+N, R = input().split()
+N, R = int(N), float(R)
+Pai = 3.1415926535
+ans = 2 * Pai * R
+p1_x, p1_y = map(float, input().split())
+f_x, f_y = p1_x, p1_y
+for i in range(N - 1):
+    p2_x, p2_y = map(float, input().split())
+    ans += math.sqrt((p1_x - p2_x) * (p1_x - p2_x) + (p1_y - p2_y) * (p1_y - p2_y))
+    p1_x, p1_y = p2_x, p2_y
+ans += math.sqrt((p1_x - f_x) * (p1_x - f_x) + (p1_y - f_y) * (p1_y - f_y))
+print('%.2f' % ans)

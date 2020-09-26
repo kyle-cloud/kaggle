3bce5055ac331adc5cb683568b7401e12e5538bd
kyle-cloud <1139467635@qq.com>
2020-09-26 10:49:16 +0800

    给相邻求圈 + 可旋转在原位置的数量,o(n)不知道为啥超时了

--- leetcode/practice.py
+++ leetcode/practice.py
@@ -1,14 +1,32 @@
-import math
+import sys
+n = int(input())
+a = [[0] * 2 for _ in range(n + 1)]
+for i in range(1, n + 1):
+    a[i][0], a[i][1] = map(int, input().split())
+#求符合的序列
+b = [0] * (n + 1)
+b[1] = 1; b[2] = a[1][0]
+t1 = b[1]; t2 = b[2]
+for i in range(3, n + 1):
+    if t1 == a[t2][0]:
+        b[i] = a[t2][1]
+    elif t1 == a[t2][1]:
+        b[i] = a[t2][0]
+    else:
+        print(-1)
+        sys.exit()
+    t1 = t2; t2 = b[i]
+if b[n] != a[1][1]:
+    print(-1)
+    sys.exit()
+#看有多少位置相同，每个数字找能够将他顺时针或逆时针放到原位的位置
+pos = [[0] * 2 for _ in range(n + 1)]
+for i in range(1, n + 1):
+    pos[(i - b[i] + 1 + n) % n][0] += 1 #顺时针
+    pos[(i + b[i] - 1) % n][1] += 1
 
-N, R = input().split()
-N, R = int(N), float(R)
-Pai = 3.1415926535
-ans = 2 * Pai * R
-p1_x, p1_y = map(float, input().split())
-f_x, f_y = p1_x, p1_y
-for i in range(N - 1):
-    p2_x, p2_y = map(float, input().split())
-    ans += math.sqrt((p1_x - p2_x) * (p1_x - p2_x) + (p1_y - p2_y) * (p1_y - p2_y))
-    p1_x, p1_y = p2_x, p2_y
-ans += math.sqrt((p1_x - f_x) * (p1_x - f_x) + (p1_y - f_y) * (p1_y - f_y))
-print('%.2f' % ans)
+ans = 0
+for i in range(n):
+    ans = max(ans, pos[i][0], pos[i][1])
+    
+print(n - ans)

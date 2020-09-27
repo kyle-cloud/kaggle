f8859e61574de61471cbdcd6a6651133e9575122
kyle-cloud <1139467635@qq.com>
2020-09-27 18:52:20 +0800

    北京2008的挂钟，还是超时，加入lru_cache结果就不对了

--- leetcode/practice.py
+++ leetcode/practice.py
@@ -1,31 +1,40 @@
-def circle():
-    n = int(input())
-    a = [[0] * 2 for _ in range(n + 1)]
-    for i in range(1, n + 1):
-        a[i][0], a[i][1] = map(int, input().split())
-    #求符合的序列
-    b = [0] * (n + 1)
-    b[1] = 1; b[2] = a[1][0]
-    t1 = b[1]; t2 = b[2]
-    for i in range(3, n + 1):
-        if t1 == a[t2][0]:
-            b[i] = a[t2][1]
-        elif t1 == a[t2][1]:
-            b[i] = a[t2][0]
-        else:
-            return -1
-        t1 = t2; t2 = b[i]
-    if b[n] != a[1][1]:
-        return -1
-    #看有多少位置相同，每个数字找能够将他顺时针或逆时针放到原位的位置
-    pos = [[0] * 2 for _ in range(n + 1)]
-    for i in range(1, n + 1):
-        pos[(i - b[i] + 1 + n) % n][0] += 1 #顺时针
-        pos[(i + b[i] - 1) % n][1] += 1
+import functools
+import time
+@functools.lru_cache()
 
-    ans = 0
-    for i in range(n):
-        ans = max(ans, pos[i][0], pos[i][1])
-    return ans    
+class Solution():
+    def __init__(self):
+        self.dir = [[1,1,0,1,1,0,0,0,0],
+                [1,1,1,0,0,0,0,0,0],
+                [0,1,1,0,1,1,0,0,0],
+                [1,0,0,1,0,0,1,0,0],
+                [0,1,0,1,1,1,0,1,0],
+                [0,0,1,0,0,1,0,0,1],
+                [0,0,0,1,1,0,1,1,0],
+                [0,0,0,0,0,0,1,1,1],
+                [0,0,0,0,1,1,0,1,1]
+            ]
+        self.start = [0] * 9
+        self.number = [0] * 9
 
-print(circle())
+    def dfs(self, index):
+        temp = self.start[:]
+        for i in range(9):
+            for j in range(9):
+                temp[i] = (temp[i] + self.dir[j][i] * self.number[j]) % 4
+        if sum(temp) == 0:
+            for i in range(9):
+                for j in range(self.number[i]):
+                    print("%d " % (i + 1), end="")
+            return
+        if index == 9:
+            return
+        for i in range(4):
+            self.number[index] = i
+            self.dfs(index + 1)
+
+solution = Solution()
+for i in range(0, 8, 3):
+    solution.start[i], solution.start[i + 1], solution.start[i + 2] = map(int, input().split())
+solution.dfs(0)
+print("")
